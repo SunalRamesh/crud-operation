a3046656d28501d833387a033e7c9cfdd29d1f2b
Sunalramesh <Sunalramesh279@gmail.com>
2022-03-17 11:33:22 +0530

    hi

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ViewProductsComponent } from './products/view-products/view-products.component';
+import { AddProductsComponent } from './products/add-products/add-products.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SpinnerComponent } from './common/spinner/spinner.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ViewProductsComponent,
+    AddProductsComponent,
+    HeaderComponent,
+    SpinnerComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
